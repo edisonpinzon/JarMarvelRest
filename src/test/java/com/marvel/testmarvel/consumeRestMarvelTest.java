33c602db529c6db78b773cca4667d8a33003fc47
edison <edison.pinzon88@gmail.com>
2023-12-02 18:15:40 -0500

    to gitHub Jar ApiMarvel

--- src/test/java/com/marvel/testmarvel/consumeRestMarvelTest.java
+++ src/test/java/com/marvel/testmarvel/consumeRestMarvelTest.java
@@ -2,17 +2,12 @@ package com.marvel.testmarvel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
